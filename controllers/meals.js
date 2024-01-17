e8688e6e2974d5dc4a5b117f5c0c8e1cb017f5dc
Jia Wang <daisy-a.119@hotmail.com>
2024-01-17 01:53:37 -0500

    set up the new controller

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,7 +1,19 @@
 import { Meal } from '../models/meal.js'
 
 function newMeal(req, res) {
-  res.send('sleepy')
+  // res.send('sleepy')
+  Meal.find({})
+  .then(meals =>{
+    res.render('meals/new', {
+      meals: meals,
+      title: 'Add Meal'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/flights')
+  })
+
 }
 
 export {
