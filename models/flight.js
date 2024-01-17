86965db38697e6a4dc31937b425fcf6c857162d8
Jia Wang <daisy-a.119@hotmail.com>
2024-01-16 23:16:08 -0500

    set up ticketSchema

--- models/flight.js
+++ models/flight.js
@@ -2,6 +2,13 @@ import mongoose from 'mongoose'
 
 // optional shortcut to the mongoose.Schema class
 const Schema = mongoose.Schema
+
+const ticketSchema = new Schema({
+  seat: {type: String, match: /[A-F][1-9]\d?/},
+  price: {type: Number, min: 0}
+}, {
+  timestamps: true
+})
 	
 const flightSchema = new Schema({
   airline: {
