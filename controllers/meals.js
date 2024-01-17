003f46a8d162bc562722a3b9269b1002a6500557
Jia Wang <daisy-a.119@hotmail.com>
2024-01-17 01:37:17 -0500

    add newMeal function

---
+++ controllers/meals.js
@@ -0,0 +1,9 @@
+import { Meal } from '../models/meal.js'
+
+function newMeal(req, res) {
+  res.send('sleepy')
+}
+
+export {
+  newMeal as new,
+}
--- routes/meals.js
+++ routes/meals.js
@@ -3,6 +3,10 @@ import * as mealsCtrl from '../controllers/meals.js'
 
 const router = Router()
 
+//GET localhost:3000/meals/new
+router.get('/new', mealsCtrl.new)
+
+
 export {
   router
 }
