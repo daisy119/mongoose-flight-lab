77d57210286ad16f8db3a4d600354e0b8cc03b60
Jia Wang <daisy-a.119@hotmail.com>
2024-01-17 02:01:13 -0500

    add reate controller

--- controllers/meals.js
+++ controllers/meals.js
@@ -13,9 +13,20 @@ function newMeal(req, res) {
     console.log(err)
     res.redirect('/flights')
   })
+}
 
+function create(req,res) {
+  Meal.create(req.body)
+  .then(meal => {
+    res.redirect('/meals/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/meals/new')
+  })
 }
 
 export {
   newMeal as new,
+  create,
 }
